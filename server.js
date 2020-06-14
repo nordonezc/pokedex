cc50dac10e389496600aa5d472fcd153df08a9f8
nordonezc <nordonezc@unal.edu.co>
2020-06-13 21:35:12 -0500

    fix: Add configuration heroku

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use(express.static(__dirname+'/dist/pokedex'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/pokedex/index.html'));
 });
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
